82b6a09e462f5a647414be0fe92c2d5c0c773429
Flv1301 <flv1301@gmail.com>
2023-04-11 14:06:50 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 
 public class Main {
@@ -21,8 +24,35 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devFlavio = new Dev();
+        devFlavio.setNome("Flávio Alexandre Souza Nunes");
+        devFlavio.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Flávio:" + devFlavio.getConteudosInscritos());
+        devFlavio.progredir();
+        devFlavio.progredir();
+        System.out.println("Conteúdos Inscritos Flávio:" + devFlavio.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Flávio:" + devFlavio.getConteudosConcluidos());
+        System.out.println("XP Flávio: " + devFlavio.calcularTotalXp());
+        System.out.println("----------------------");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Outro Dev");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Outro Dev:" + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteúdos Inscritos Outro Dev:" + dev2.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Outro Dev:" + dev2.getConteudosConcluidos());
+        System.out.println("XP Outro Dev: " + dev2.calcularTotalXp());
     }
 }
